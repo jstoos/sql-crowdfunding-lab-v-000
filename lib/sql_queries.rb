c4593dd063be414495c87b0c21e32a9feb47b6ab
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-21 00:45:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 FROM projects
 LEFT JOIN pledges
 ON pledges.project_id = projects.id
-HAVING category = 'music'
+WHERE category = 'music'
 "
 end
 
